02f0740faa70ecfcbfea419436532dd4622bb4bf
ssgarx <sagarchandgadkar74@gmail.com>
2021-12-19 15:31:51 +0530

    central rail update

--- client/src/pages/Feed/MainFeed.js
+++ client/src/pages/Feed/MainFeed.js
@@ -4,7 +4,20 @@ import style from "./mainfeed.module.scss";
 function MainFeed() {
   return (
     <div className={style.box1}>
-      <h1>Main feeed</h1>
+      <div className={style.feedRail}>
+        <div className={style.feedinfobox}>
+          <div>
+            <p>
+              Most upLysted ones here <span role="img">👇</span>
+            </p>
+          </div>
+          <div>
+            <button>By Uplysts</button>
+            <button>By Newest</button>
+            <button>By Vibe</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
