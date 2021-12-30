314938c6065d91d9a53ae3b5c8658801bae452eb
ssgarx <sagarchandgadkar74@gmail.com>
2021-12-30 23:04:55 +0530

    login form fix

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://mysterious-citadel-06025.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
